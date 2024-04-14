9fb656b6d4af6b2b6ce690eb91a86d249d49eb80
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-04-14 16:50:47 -0500

    Start learning C

---
+++ dev/programming_languages/c/starting_to_write_code/first_program/first_program.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int my_favorite_number = 0;
+
+    printf("Please enter your favorite number\n");
+    scanf("%d", &my_favorite_number);
+    printf("You entered number: %d\n", my_favorite_number);
+
+    return 0;
+}
